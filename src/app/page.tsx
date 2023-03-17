e9c4061c32f9023623d2b2e03a716ec9864ae952
hmai2139 <hoangmai2139@gmail.com>
2023-03-17 21:15:16 +1100

    Added typescript to dev dependencies and added .css files declaration.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+import * as styles from './page.module.css'
 import NavBar from './components/navbar'
 
 const inter = Inter({ subsets: ['latin'] })
